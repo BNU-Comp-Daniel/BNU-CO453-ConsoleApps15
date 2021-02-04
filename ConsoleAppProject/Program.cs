3e984a13f2efada15fda210de2afa555be5b481f
DanielHale99 <72168379+DanielHale99@users.noreply.github.com>
2021-02-04 00:35:11 +0000

    added heading

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -16,10 +16,18 @@ namespace ConsoleAppProject
         public static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
-            
+
             Console.WriteLine("BNU CO453 Applications Programming 2020-2021!");
             Console.WriteLine();
             Console.Beep();
+
+            
+                Console.WriteLine();
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("      Convert Miles to Feet       ");
+                Console.WriteLine("          by Daniel Hale          ");
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine();
 
             DistanceConverter converter = new DistanceConverter();
             converter.Run();
