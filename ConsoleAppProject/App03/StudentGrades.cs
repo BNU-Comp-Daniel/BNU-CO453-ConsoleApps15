26100eb72ddf257ef407ba3da2fc3257af0ed030
DanielHale99 <72168379+DanielHale99@users.noreply.github.com>
2021-03-17 11:06:31 +0000

    Added methods

--- ConsoleAppProject/App03/StudentGrades.cs
+++ ConsoleAppProject/App03/StudentGrades.cs
@@ -4,7 +4,64 @@ using System.Text;
 
 namespace ConsoleAppProject.App03
 {
-    class StudentGrades
+    public class StudentGrades
     {
+        public string [] Students { get; set; }
+
+        public int[] Marks { get; set; }
+
+        public int[] GradeProfile { get; set; }
+
+        public double Mean { get; set; }
+
+        public int Minimum { get; set; }
+
+        public int Maximum { get; set; }
+
+        public StudentGrades()
+        {
+            Students = new string[]
+                    (
+                "Daniel", "Dylan", "Eric",
+                "Georgia", "Hasan", "Hamza",
+                "Jack", "Liam", "Shan",
+                "Shamial"
+                    );
+            GradeProfile = new int[(int)Grades.A + 1];
+            Marks = new int[Students.Length];
+        }
+
+       /// <summary>
+       /// Enter in the marks of the students.
+       /// </summary>
+        public void InputMarks()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// List all of the student's current marks.
+        /// </summary>
+        public void OutputMarks()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Convert the marks to a grade.
+        /// </summary>
+        public Grades ConvertToGrade(int mark)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Calculate and show the mean, minimum, maximum marks
+        /// for the students.
+        /// </summary>
+        public void CalculateStats()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
